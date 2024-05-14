c8b62ac1f4e8219eb9672a62348584416917c55c
javid shabin <javid.main.act@gmail.com>
2024-05-14 23:39:06 +0530

    add rotating style for the contents

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ let day = new Date()
 let hh = day.getHours() * 30;
 let mm = day.getMinutes() * deg;
 let ss = day.getSeconds() * deg;
+
+hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
