316090556f1872d9a13591a053d22a5c22704a26
javid shabin <javid.main.act@gmail.com>
2024-05-14 23:41:45 +0530

    add a setInterVal function for the clock. The the style aoutomaticlay exicuted and complted the clock function

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const hr = document.getElementById("hr");
 const mn = document.getElementById("mn");
 const sc = document.getElementById("sc");
 
-let day = new Date()
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
 
-hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-mn.style.transform = `rotateZ(${mm}deg)`;
-sc.style.transform = `rotateZ(${ss}deg)`;
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
